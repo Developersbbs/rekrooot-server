0397c14892532688bc1bbd1f7cbd4dc67416c28d
Vishali <vishakannan07@gmail.com>
2026-05-23 13:04:35 +0530

    config

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import { ENV } from "./env.js";
 
 export async function connectDB() {
   try {
-    await mongoose.connect(ENV.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
